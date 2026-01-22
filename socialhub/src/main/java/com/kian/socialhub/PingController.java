4a0fdb7366cfedaac69c4200302f5cadea3cd4bd
Kian-Houltram <your_github_email@example.com>
2026-01-22 16:08:55 +0000

    Fixing names of files

---
+++ socialhub/src/main/java/com/kian/socialhub/PingController.java
@@ -0,0 +1,13 @@
+package com.kian.socialhub;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ok";
+    }
+}
