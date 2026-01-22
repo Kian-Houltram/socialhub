54b1f72401b665645b03e59095ae1cab92ffb147
Kian-Houltram <your_github_email@example.com>
2026-01-22 14:00:52 +0000

    setting up environment with spring boot

---
+++ socialhub/src/test/java/com/kian/socialhub/SocialhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kian.socialhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
